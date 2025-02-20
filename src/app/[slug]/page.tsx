b3adca34d0c21098f954d539d6d415d3ee51ae55
kenidydrosa <kenidydrosa@gmail.com>
2025-02-20 13:11:02 -0300

    feat: add-image-slug-restaurant

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,6 @@
-import { db } from "@/lib/prisma";
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -6,8 +8,23 @@ interface RestaurantPageProps {
 
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
-  const restaurant = await db.restaurant.findUnique({ where: { slug } });
-  return <h1>{restaurant?.name}</h1>;
+  const restaurant = await getRestaurantBySlug(slug);
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="h-scren flex flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant?.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant?.name}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantPage;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
+// import { Button } from "@/components/ui/button";
 const ProductsPage = () => {
   return (
-    <div className='p-10'>
-      <h1 className='text-red-500'>Products page</h1>
-      <Button>Click me</Button>
+    <div className="p-10">
+      <h1 className="text-red-500">Products page</h1>
+      {/* <Button>Click me</Button> */}
     </div>
   );
 };
