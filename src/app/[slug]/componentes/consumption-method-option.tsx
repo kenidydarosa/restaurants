c0d68bf9ec468b3f1e94c3eae6f441952fc56625
kenidydrosa <kenidydrosa@gmail.com>
2025-02-21 13:00:57 -0300

    feat: create-menu-page

--- src/app/[slug]/componentes/consumption-method-option.tsx
+++ src/app/[slug]/componentes/consumption-method-option.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+import { ConsumptionMethod } from "@prisma/client";
 
 interface ConsumptionMethodOptionProps {
+  slug: string;
   imageUrl: string;
   imageAlt: string;
   buttonText: string;
+  option: ConsumptionMethod;
 }
 
 const ConsumptionMethodOption = ({
+  slug,
   imageUrl,
   imageAlt,
   buttonText,
+  option
 }: ConsumptionMethodOptionProps) => {
   return (
     <Card>
@@ -26,7 +32,7 @@ const ConsumptionMethodOption = ({
           />
         </div>
         <Button variant={"secondary"} className="rounded-full">
-          {buttonText}
+          <Link href={`/${slug}/menu?consumptionMethod=${option}`}>{buttonText}</Link>
         </Button>
       </CardContent>
     </Card>
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -26,20 +26,23 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
         <h2 className="font-semibold">{restaurant?.name}</h2>
       </div>
       <div className="space-y-2 pt-24 text-center">
-        <h3 className="text-2xl font-semibold">
-          <p className="opacity-55">
-            Escolha como prefere aproveitar sua refeição. Estamos felizes em
-            oferecer praticidadde e sabor em cada detalhe!
-          </p>
-        </h3>
+        <h3 className="text-2xl font-semibold">Seja bem vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos felizes em
+          oferecer praticidadde e sabor em cada detalhe!
+        </p>
       </div>
       <div className="grid grid-cols-2 gap-4 pt-14">
         <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
           imageUrl="/dine_in.png"
           imageAlt="Comer aqui"
           buttonText="Para comer aqui"
         />
         <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
           imageUrl="/takeway.png"
           imageAlt="Para levar"
           buttonText="Para levar"
